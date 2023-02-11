de97b5ad90dd367a6b1b23838028043b59c8815b
sandeep reddy <sandeepreddyr12@gmail.com>
2023-02-11 13:44:58 +0530

    frontend implemented

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,25 @@
-// with revalidate it acts like ISR
+import React from 'react';
+import Chat from './chat';
 
-function homepage() {
-  return <div>hi thereffff</div>;
+type Props = {};
+
+export default function page({}: Props) {
+  return (
+    <div>
+      <div className="menu">
+        <div className="back">
+          <i className="fa fa-chevron-left" />
+          {/* <img src="https://i.imgur.com/DY6gND0.png" draggable="false" /> */}
+        </div>
+        <div className="name">Hello world</div>
+        <div className="last">18:09</div>
+      </div>
+      <Chat />
+      {/* <input className="textarea" type="text" placeholder="Type here!" /> */}
+      <div className="emojisss">
+        <img src="app\attachIcon.png" alt="attach" />
+      </div>
+      <div>send</div>
+    </div>
+  );
 }
-
-export default homepage;
